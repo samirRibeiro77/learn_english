b3ff7207152a297f3905237d4e05f5a6bcac9caf
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-09 14:38:09 -0300

    Numbers tab

--- lib/ui/tabs/numbers_tab.dart
+++ lib/ui/tabs/numbers_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_english/helper/media_helper.dart';
 
 class NumbersTab extends StatefulWidget {
   const NumbersTab({super.key});
@@ -8,13 +9,47 @@ class NumbersTab extends StatefulWidget {
 }
 
 class _NumbersTabState extends State<NumbersTab> {
+  final _mediaHelper = MediaHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mediaHelper.cache = [
+      "1", "2", "3", "4", "5", "6",
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
       children: [
-        Row(),
-        Row(),
-        Row()
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("1"),
+          child: Image.asset("assets/images/1.png"),
+        ),
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("2"),
+          child: Image.asset("assets/images/2.png"),
+        ),
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("3"),
+          child: Image.asset("assets/images/3.png"),
+        ),
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("4"),
+          child: Image.asset("assets/images/4.png"),
+        ),
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("5"),
+          child: Image.asset("assets/images/5.png"),
+        ),
+        GestureDetector(
+          onTap: () => _mediaHelper.playMp3("6"),
+          child: Image.asset("assets/images/6.png"),
+        ),
       ],
     );
   }
