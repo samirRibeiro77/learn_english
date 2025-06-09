ff360237dd43cfbc3b46062067e6c34ec6f345bb
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-09 13:03:38 -0300

    Add files, create animals tab ui

--- lib/ui/tabs/animals_tab.dart
+++ lib/ui/tabs/animals_tab.dart
@@ -10,11 +10,34 @@ class AnimalsTab extends StatefulWidget {
 class _AnimalsTabState extends State<AnimalsTab> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
       children: [
-        Row(),
-        Row(),
-        Row()
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/vaca.png"),
+        ),
       ],
     );
   }
